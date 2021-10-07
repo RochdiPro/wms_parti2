f0f0afcd43b675984dac2680f5eb280dbc3c8762
fathiazidi <fathiazidi9@gmail.com>
2021-10-07 11:24:01 +0100

    commit 07/10/2021

--- src/app/WMS/Stockage/tableau/tableau.component.ts
+++ src/app/WMS/Stockage/tableau/tableau.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hall } from '../../Classe/Stockage/Hall';
 import { Rayon } from '../../Classe/Stockage/Rayon';
 import { StockageService } from '../stockage.service';
 
@@ -10,15 +11,25 @@ import { StockageService } from '../stockage.service';
 export class TableauComponent implements OnInit {
 
   constructor(private service: StockageService) { }
+  hall:Hall=new Hall()
+
   x: number
   y: number
   arr: any[]=[];
-  rayons: Rayon[] = []
-  ngOnInit(): void {
+   halles: any = [];
+  rayons: any = [];
+  emplacmentselect: any
+  libelleLocal: any
+  local: any
+  rayonShow:boolean=false
+  hallShow:boolean=true
+   libelleHalle:any
+   ngOnInit(): void {
     this.arr=[]
-
     this.service.getHallById(1).subscribe(data => {
       this.rayons = data.rayons
+      this.hall=data
+
       console.log(this.rayons)
       this.service.MaxOrdreX(1).subscribe(data => {
         this.x = data;
@@ -26,7 +37,7 @@ export class TableauComponent implements OnInit {
           this.y = data;
           console.log("y", this.y)
           console.log("x", this.x)
-          this.generertableayrayon()
+          this.generertableayrayon(this.hall)
         }, error => console.log(error));
       }, error => console.log(error));
     }, error => console.log(error));
@@ -36,38 +47,45 @@ export class TableauComponent implements OnInit {
   counter(i: number) {
     return new Array(i);
 }
-  generertableayrayon() {
-var i=0
-var j=0
+generertableayrayon(halle: any) {
+  this.arr = []
+  console.log("eeee", halle)
+  for (let i = 0; i < this.x; i++) {
+    // Creates an empty line
+    this.arr.push([]);
+    // Adds cols to the empty line:
+    this.arr[i].push(new Array(this.y));
+    for (let j = 0; j < this.y; j++) {
+      this.service.OrdreRayonExiste(halle.id, i + 1, j + 1).subscribe(data => {
+        console.log(" eee", data)
+        if (data != null) {
+          this.arr[i][j] = data;
+        }
+        else {
+          //ordre n'exsite pas
+          this.service.ZoneExiste(halle.id, i + 1, j + 1).subscribe(data => {
+            console.log(" eee", data)
+            if (data == true) {
+              this.arr[i][j] = "invalide";
+            }
+            else {
+              this.arr[i][j] = null;
+            }
+          }, error => console.log(error));
 
-    for (let i=0 ; i < this.x; i++) {
-      // Creates an empty line
-      this.arr.push([]);
-      // Adds cols to the empty line:
-      this.arr[i].push(new Array(this.y));
-      for ( let j =0 ; j < this.y; j++) {
-        
-        this.service.OrdreRayonExiste(1, i+1,j+1).subscribe(data => {
-          console.log(" eee", data)
-          if (data != null) {
-            this.arr[i][j] = data;
-          }
-          else
-           {
-            this.arr[i][j] = null;
-           }
-        } , error => console.log(error));
-    
- 
-      }
-   
-      }
-      setTimeout(() => {
-        console.log("array", this.arr);
-      }, 2000);
-    
+
+
+        }
+      }, error => console.log(error));
+
     }
 
+  }
+  setTimeout(() => {
+    console.log("array", this.arr);
+  }, 2000);
+
+}
 
   generertableayrayonV0() {
 
@@ -97,6 +115,22 @@ var j=0
       }
     }
     console.log("fff", this.arr);
+  }
+  SelectHall(hall:any){
+    this.rayonShow=true
+    this.hallShow=false
+    this.libelleHalle=hall.libelle
+    this.hall=hall
+    this.rayons=hall.rayons
+    this.service.MaxOrdreX(hall.id).subscribe(data => {
+      this.x = data;
+      this.service.MaxOrdreY(hall.id).subscribe(data => {
+        this.y = data;
+        console.log("y", this.y)
+        console.log("x", this.x)
+        this.generertableayrayon(hall)
+      }, error => console.log(error));
+    }, error => console.log(error)); 
   }
 
 }
